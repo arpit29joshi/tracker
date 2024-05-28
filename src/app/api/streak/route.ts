2736580cbfa5f6181519467fba3404acf59ddfd9
arpit29joshi <arpit29joshi@gmail.com>
2024-05-28 20:23:19 +0530

    add secretKey in streak api

--- src/app/api/streak/route.ts
+++ src/app/api/streak/route.ts
@@ -6,30 +6,45 @@ connect();
 
 export async function PUT(request: NextRequest) {
   try {
-    // 1. Update user streaks
-    const users = await User.find();
-    for (const user of users) {
-      if (user.isAllTasksCompleted) {
-        user.currentStreak += 1;
-        if (user.currentStreak > user.longestStreak) {
-          user.longestStreak = user.currentStreak;
-        }
-      } else {
-        user.currentStreak = 0;
-      }
-      user.isAllTasksCompleted = false; // Reset the flag for the next day
-      await user.save();
+    const reqBody = await request.json();
+    const { secretKey } = reqBody;
+    console.log("secretKey", secretKey);
+    console.log("AUTH_SECRET", process.env.AUTH_SECRET);
+    if (!secretKey) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 400 });
     }
-
-    // 2. Reset all tasks
-    await Task.updateMany({}, { isCompleted: false });
-
-    return NextResponse.json(
-      {
-        message: "Daily task completed successfully.",
-      },
-      { status: 201 }
-    );
+    if (secretKey !== process.env.AUTH_SECRET)
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    if (secretKey === process.env.AUTH_SECRET) {
+      return NextResponse.json(
+        {
+          message: "Daily task completed successfully.",
+        },
+        { status: 201 }
+      );
+      // // 1. Update user streaks
+      // const users = await User.find();
+      // for (const user of users) {
+      //   if (user.isAllTasksCompleted) {
+      //     user.currentStreak += 1;
+      //     if (user.currentStreak > user.longestStreak) {
+      //       user.longestStreak = user.currentStreak;
+      //     }
+      //   } else {
+      //     user.currentStreak = 0;
+      //   }
+      //   user.isAllTasksCompleted = false; // Reset the flag for the next day
+      //   await user.save();
+      // }
+      // // 2. Reset all tasks
+      // await Task.updateMany({}, { isCompleted: false });
+      // return NextResponse.json(
+      //   {
+      //     message: "Daily task completed successfully.",
+      //   },
+      //   { status: 201 }
+      // );
+    }
   } catch (error: any) {
     console.error("Error in API call: ", error);
     return NextResponse.json({ error: error.message }, { status: 500 });
